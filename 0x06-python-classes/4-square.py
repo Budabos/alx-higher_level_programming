f8b3899b23bb96310e66a410799a4129c8f51eb3
Budabos <opiyocaroline20@gmail.com>
2023-01-24 22:21:21 +0000

    0x06-python-classes

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,48 +1,37 @@
 #!/usr/bin/python3
-"""Defines a class Square"""
+
+"""a class that creates a square"""
 
 
 class Square:
-    """Represents a square
-    Attributes:
-        __size (int): size of a side of the square
-    """
+    """the square class"""
+
     def __init__(self, size=0):
-        """initializes the square
-        Args:
-            size (int): size of a side of the square
-        Returns:
-            None
+        """initialize a new square
+	
+	Args:
+            size (int): the size of the square
         """
         self.size = size
 
     def area(self):
-        """calculates the square's area
+        """class method that gets the area of square
         Returns:
-            The area of the square
+            result
         """
-        return (self.__size) ** 2
+
+        result = self.__size * self.__size
+        return (result)
 
     @property
     def size(self):
-        """getter of __size
-        Returns:
-            The size of the square
-        """
-        return self.__size
+        """class method that retrieve a private instance attribute"""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        """setter of __size
-        Args:
-            value (int): the size of a size of the square
-        Returns:
-            None
-        """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        else:
-            if value < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = value
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
