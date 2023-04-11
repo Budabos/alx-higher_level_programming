3be58c9c28b0e8fd2863e372f5394d4b97b24a50
Budabos <opiyocaroline20@gmail.com>
2023-04-11 12:05:33 +0000

    javascript

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const count = 'process.argv.length';
+const argv = 'process.argv.length';
 if (process.argv.length < 3) {
   console.log('No argument');
 } else if (process.argv.length > 3) {
