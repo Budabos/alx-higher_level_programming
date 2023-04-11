e0a38d57b41c3a2958df3c5d048d6ce119c06816
Budabos <opiyocaroline20@gmail.com>
2023-04-11 19:03:18 +0000

    javascript

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,6 @@
 #!/usr/bin/node
+function factorial (n) {
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
+  }
 
-const args = process.argv.slice(2);
-
-function fact(n) {
-  if (isNaN(n)) return 1;
-  if (n === 0) return 1;
-  const result = n * fact(n - 1);
-  return result;
-}
-
-const num = Number(args[0]);
-
-console.log(fact(num));
+  console.log(factorial(Number(process.argv[2])));
