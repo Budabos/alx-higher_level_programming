4db71a13da65965d2cecfbbf312fcc9706d85638
Budabos <opiyocaroline20@gmail.com>
2023-04-11 10:29:47 +0000

    task 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,2 @@
 #!/usr/bin/node
-const  = "C is fun";
-const = "Python is cool";
-const = "JavaScript is amazing";
-colsole.log("C is fun" "Python is cool" "JavaScript is amazing");
+colsole.log("C is fun"\n"Python is cool"\n"JavaScript is amazing");
