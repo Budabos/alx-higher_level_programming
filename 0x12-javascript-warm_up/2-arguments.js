0cead8fa3775d3057c5b24a20c4b254f51fbcf47
Budabos <opiyocaroline20@gmail.com>
2023-04-11 11:09:12 +0000

    javascript-warm_up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const argument = n;
-if(n == 0);
+if(n === 0);
 console.log('No argument');
-if else(n ==1)
+if else(n === 1)
 Console.log('Argument found');
 else (n < 1 );
 console.log('Arguments found');
